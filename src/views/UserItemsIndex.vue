bcb2ef09c8fd41d4bf5b7289e8fead7a45baf339
PeterH <pete08@gmail.com>
2020-08-22 16:31:28 -0500

    Add create action button for user_item in UserItemsIndex.vue page

--- src/views/UserItemsIndex.vue
+++ src/views/UserItemsIndex.vue
@@ -4,6 +4,15 @@
     <br>
     <router-link v-bind:to="`/groceries`">See your Grocery list</router-link>
     <hr>
+    
+    <h1> new item: </h1>
+    <div>
+      name: <input type="text" v-model="name" />
+      item id: <input type="text" v-model="item_id" />
+      <button v-on:click="addItem()">Add item!</button>
+    </div>
+    
+    <hr>
     <div v-for="item in user_items">
     <ul>
       <br>
@@ -26,20 +35,30 @@ export default {
     return {
       message: "Welcome to you Pantry on the Go!",
       user_items: {} ,
-    };
+      item_id: "",
+      name: "",
+    } ;
   },
   
   created: function() {
     axios.get("/api/user_items").then(response=> { // Create USerITemsShow page based on current user logged in
       console.log("user_item : ", response) ;
       this.user_items = response.data ;
-    });
+    }) ;
   },
 
   methods: {
     
     addItem: function() {
-
+      var params = {
+        name: this.name,
+        item_id: this.item_id,
+      }
+      axios.post("/api/user_items", params).then(response=> {
+        console.log("made item!") ; 
+        console.log("new user_item: ", response) ; 
+        this.user_item = response.data ;
+      }) ;
     }
   }
 };
